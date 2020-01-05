8be5fed0c8592836e06d0d8e710ea71002e6c40b
Pierre-Do <pierredominique.putallaz@gmail.com>
2020-01-05 13:32:30 +0000

    Updates

---
+++ precache-manifest.6d5b6b8b8974466ba7311783469239a8.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae4eba14bc0732e4825422ea73e5180",
+    "url": "/carousel/index.html"
+  },
+  {
+    "revision": "963a15c144f24304b0b6",
+    "url": "/carousel/static/js/2.5e4b64b3.chunk.js"
+  },
+  {
+    "revision": "43e75541cb2c99a81f43877710dd8732",
+    "url": "/carousel/static/js/2.5e4b64b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6df5ec8736651b9c1328",
+    "url": "/carousel/static/js/main.63e3155b.chunk.js"
+  },
+  {
+    "revision": "c9dab4efce89e88da00a",
+    "url": "/carousel/static/js/runtime-main.4cdf4280.js"
+  },
+  {
+    "revision": "43e4c49eee0462d7b7e9edbe04553e81",
+    "url": "/carousel/static/media/1.43e4c49e.jpg"
+  },
+  {
+    "revision": "67ed89710be25224d0aaf7113f95d6c4",
+    "url": "/carousel/static/media/1.67ed8971.webp"
+  },
+  {
+    "revision": "0191b0ab70f16f32f81c36e4e043b522",
+    "url": "/carousel/static/media/10.0191b0ab.webp"
+  },
+  {
+    "revision": "61757b73408e64855afe3bba92f9d0eb",
+    "url": "/carousel/static/media/10.61757b73.jpg"
+  },
+  {
+    "revision": "b2f2c7a89ff76c3205341558c6582a21",
+    "url": "/carousel/static/media/10_small.b2f2c7a8.jpg"
+  },
+  {
+    "revision": "be2f2418bdd5173a04681fe7d1ac5a6b",
+    "url": "/carousel/static/media/1_small.be2f2418.jpg"
+  },
+  {
+    "revision": "b88dec3c0502b1aa073485fbe0eeb513",
+    "url": "/carousel/static/media/2.b88dec3c.webp"
+  },
+  {
+    "revision": "f7d199514a182d72abcbbad34241a20e",
+    "url": "/carousel/static/media/2.f7d19951.jpg"
+  },
+  {
+    "revision": "a73f1ef226635045ffd1ac7556d07aec",
+    "url": "/carousel/static/media/2_small.a73f1ef2.jpg"
+  },
+  {
+    "revision": "a2d9f43182293df3fc1ff520f15d4979",
+    "url": "/carousel/static/media/3.a2d9f431.webp"
+  },
+  {
+    "revision": "dc8a3344a9b36bed14da61043f4a08f2",
+    "url": "/carousel/static/media/3.dc8a3344.jpg"
+  },
+  {
+    "revision": "4f138faf7103f983a628e9e84efb7fd4",
+    "url": "/carousel/static/media/3_small.4f138faf.jpg"
+  },
+  {
+    "revision": "a9865d274fa4cb1e7f7cfc9552bdf8a8",
+    "url": "/carousel/static/media/4.a9865d27.webp"
+  },
+  {
+    "revision": "e2df36ff3ba36e2b954abb8aad514353",
+    "url": "/carousel/static/media/4.e2df36ff.jpg"
+  },
+  {
+    "revision": "c8175740c28bd483d712dc668c699b06",
+    "url": "/carousel/static/media/4_small.c8175740.jpg"
+  },
+  {
+    "revision": "0b4000270d74113e4cb67ead3c8d0123",
+    "url": "/carousel/static/media/5.0b400027.jpg"
+  },
+  {
+    "revision": "85a97d376504033e17d57c358362e35c",
+    "url": "/carousel/static/media/5.85a97d37.webp"
+  },
+  {
+    "revision": "1856f0ceab61d70d09c9a192002d94bb",
+    "url": "/carousel/static/media/5_small.1856f0ce.jpg"
+  },
+  {
+    "revision": "32cfee3f05e29a96ba05f31b97e296fb",
+    "url": "/carousel/static/media/6.32cfee3f.jpg"
+  },
+  {
+    "revision": "63f3c704889c953649e3afe9c3a5f8a3",
+    "url": "/carousel/static/media/6.63f3c704.webp"
+  },
+  {
+    "revision": "a7359d23864665d4cfd3aa0fdffe434f",
+    "url": "/carousel/static/media/6_small.a7359d23.jpg"
+  },
+  {
+    "revision": "351280410408728c716ab988650c096a",
+    "url": "/carousel/static/media/7.35128041.jpg"
+  },
+  {
+    "revision": "b55b207490d8a0c170f9bf88dc28cfda",
+    "url": "/carousel/static/media/7.b55b2074.webp"
+  },
+  {
+    "revision": "ba4c709952a607f0d44bf03ab5ecbb5a",
+    "url": "/carousel/static/media/7_small.ba4c7099.jpg"
+  },
+  {
+    "revision": "67a0cb424fbd18cccefcf6d250c9f771",
+    "url": "/carousel/static/media/8.67a0cb42.jpg"
+  },
+  {
+    "revision": "fa188ed5afdd0aecc80bf12efcc28c04",
+    "url": "/carousel/static/media/8.fa188ed5.webp"
+  },
+  {
+    "revision": "d730c0757b837508fcfac8258c3868de",
+    "url": "/carousel/static/media/8_small.d730c075.jpg"
+  },
+  {
+    "revision": "175bc0d70c4cef935770b0b20156f57e",
+    "url": "/carousel/static/media/9.175bc0d7.webp"
+  },
+  {
+    "revision": "d09e100b51e448f70ab3ec7ddf0567b7",
+    "url": "/carousel/static/media/9.d09e100b.jpg"
+  },
+  {
+    "revision": "4bd2bb579fb2c7f1c1a42a35e7d70b11",
+    "url": "/carousel/static/media/9_small.4bd2bb57.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/carousel/precache-manifest.a251601457158dc58bbbaa059a1bd592.js"
+  "/carousel/precache-manifest.6d5b6b8b8974466ba7311783469239a8.js"
 );
 
 self.addEventListener('message', (event) => {
