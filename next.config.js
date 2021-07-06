87e4c7209516240ec48690dbbfff3b0883ba70b7
Pierre-Dominique Putallaz <pierre-dominique.putallaz@qoqa.com>
2021-07-06 11:57:10 +0200

    Use image optimization to add a prefix to their path

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ module.exports = {
   // Remove this to leverage Next.js' static image handling
   // read more here: https://nextjs.org/docs/api-reference/next/image
   images: {
-    disableStaticImages: true,
     path: process.env.NEXT_PUBLIC_BASE_PATH,
   },
 };
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,52 @@
-declare module '*.jpeg';
-declare module '*.jpg';
-declare module '*.png';
-declare module '*.svg';
-declare module '*.ico';
-declare module '*.webp';
+declare module '*.png' {
+  const content: StaticImageData;
+
+  export default content;
+}
+
+declare module '*.svg' {
+  /**
+   * Use `any` to avoid conflicts with
+   * `@svgr/webpack` plugin or
+   * `babel-plugin-inline-react-svg` plugin.
+   */
+  const content: any;
+
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: StaticImageData;
+
+  export default content;
+}
+
+declare module '*.jpeg' {
+  const content: StaticImageData;
+
+  export default content;
+}
+
+declare module '*.gif' {
+  const content: StaticImageData;
+
+  export default content;
+}
+
+declare module '*.webp' {
+  const content: StaticImageData;
+
+  export default content;
+}
+
+declare module '*.ico' {
+  const content: StaticImageData;
+
+  export default content;
+}
+
+declare module '*.bmp' {
+  const content: StaticImageData;
+
+  export default content;
+}
