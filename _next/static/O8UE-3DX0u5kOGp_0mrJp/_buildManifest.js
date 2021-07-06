ada23729378eb11b36732a19ed90a000ac896940
Pierre-Do <dev+github-chappie@qoqa.com>
2021-07-06 09:59:30 +0000

    Updates

--- _next/static/O8UE-3DX0u5kOGp_0mrJp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-d8ab039462ca1f81e4ed.js"],"/[[...slug]]":["static/chunks/pages/[[...slug]]-f8abe9e63fa3dfd9fb21.js"],sortedPages:["/_app","/_error","/[[...slug]]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
